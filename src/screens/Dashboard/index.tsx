0684c0a773ff57f4bac702cad537887e75ba6d33
Ulisses <m.stunik@gmail.com>
2021-10-04 09:49:59 -0300

    create header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-import { Container, Title } from './styles'
+import { Container, Header, UserInfo, Photo, User, UserGreeting, UserName, UserWrapper, Icon } from './styles'
+import { RFValue } from 'react-native-responsive-fontsize'
 
 export function Dashboard() {
     return <Container >
-        <Title>Dashboard</Title>
+        <Header>
+            <UserWrapper>
+                <UserInfo>
+                    <Photo source={{ uri: 'http://github.com/yowlisses.png' }} />
+                    <User>
+                        <UserGreeting>Olá, </UserGreeting>
+                        <UserName>Ulisses</UserName>
+                    </User>
+                </UserInfo>
+                <Icon name="power" />
+            </UserWrapper>
+        </Header>
     </Container>
 }
